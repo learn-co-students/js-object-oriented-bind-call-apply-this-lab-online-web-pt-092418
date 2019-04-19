8e0271375c9e4cf4223b7b823a0bc08c296132a0
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-18 21:06:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arguments) {
+  return fn.call(thisValue, arguments);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copyFun =  functionToBeCopied.bind(thisValue);
+    return copyFun;
+}
